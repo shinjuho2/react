a061c68e2b1c97e89c6fa3e4b087d30adf395866
shinjuho2 <sinjuho2@gmail.com>
2023-10-10 23:00:50 +0900

    2일차

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+/* esLint-disable*/
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '서울'
+  let [글제목, 글제목1] = useState(['파이썬', '자바', '물고기']);
+  let [good, good1] = useState(0);
+
 
   return (
     <div className="App">
       <div className='black-nav'>
         <h4>blog</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className='list'>
+        <h4>{ 글제목[0] } <span onClick={ () => { good1(good + 1) } }>👍</span> { good } </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목[1] } <span onClick={ () => { 글제목('C언어') } }>🔄️</span></h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
