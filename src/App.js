949461faa67efbc00084b85c81d22953d5344f33
shinjuho2 <sinjuho2@gmail.com>
2023-10-12 23:58:41 +0900

    4일차

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 function App() {
 
   let post = '서울'
-  let [글제목, 글제목1] = useState(['파이썬', '자바', '물고기']);
+  let [글제목, 글제목1] = useState(['b', 'a', 'c']);
   let [good, good1] = useState(0);
 
 
@@ -16,16 +16,21 @@ function App() {
       <div className='black-nav'>
         <h4>blog</h4>
       </div>
+      <button onClick={ ()=>{ 
+      let copy = [...글제목];
+      copy.sort();
+      글제목1(copy)
+    } }> 정렬버튼 </button>
       <div className='list'>
-        <h4>{ 글제목[0] } <span onClick={ () => { good1(good + 1) } }>👍</span> { good } </h4>
+        <h4>{글제목[0]} <span onClick={() => { good1(good + 1) }}>👍</span> {good} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{ 글제목[1] } <span onClick={ () => { 글제목1(['파이썬', 'C언어', '물고기']) } }>🔄️</span></h4>
+        <h4>{글제목[1]} <span onClick={() => { 글제목1(['python', 'C', 'fish']) }}>🔄️</span></h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{ 글제목[2] }</h4>
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
