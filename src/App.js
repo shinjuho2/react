ac5af593059098c70f926fec09768943c921e5f1
shinjuho2 <sinjuho2@gmail.com>
2023-10-14 21:57:40 +0900

    6일

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   let post = '서울'
   let [글제목, 글제목1] = useState(['python', 'java', 'Go']);
   let [good, good1] = useState(0);
+  let [modal, setModal] = useState(0)
 
 
   return (
@@ -16,11 +17,11 @@ function App() {
       <div className='black-nav'>
         <h4>blog</h4>
       </div>
-      <button onClick={ ()=>{ 
-      let copy = [...글제목];
-      copy.sort();
-      글제목1(copy)
-    } }> 정렬버튼 </button>
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy.sort();
+        글제목1(copy)
+      }}> 정렬버튼 </button>
       <div className='list'>
         <h4>{글제목[0]} <span onClick={() => { good1(good + 1) }}>👍</span> {good} </h4>
         <p>2월 17일 발행</p>
@@ -30,11 +31,26 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={() => { setModal(!modal) }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      {
+        modal == 1 ? <Modal></Modal> : null
+      }
+
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
